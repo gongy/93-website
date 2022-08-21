8474f7d1a46b18869c35522b00a3e323102c501a
Richard Gong <richard@modal.com>
2022-08-21 10:22:46 -0400

    frontend

---
+++ frontend/src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Card from './lib/Card.svelte'
+</script>
+
+<main>
+  <h1 class="text-center p-10 text-zinc-100 text-3xl font-light">
+    93 Leonard
+  </h1>
+  <div class="grid grid-cols-2 max-w-lg">
+    <Card class="py-5">Guests</Card>
+    <Card>Laundry</Card>
+    <Card>Packages</Card>
+  </div>
+</main>
---
+++ frontend/src/lib/Card.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<button class="m-2 p-8 text-zinc-100 rounded-md bg-gray-600 hover:bg-gray-500">
+  <slot/>
+</button>
