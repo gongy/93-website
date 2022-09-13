8daab1e871de575528b9cc0e76b9ca9dd49fb40a
Richard Gong <richard@modal.com>
2022-09-13 15:48:57 -0400

    hour formatting

--- frontend/src/Home.svelte
+++ frontend/src/Home.svelte
@@ -8,14 +8,20 @@
       return Math.floor((utcTs() / 1000 - t) / 60);
     }
 
+    function simpleFormatMins (x) {
+      if (x < 120) return `${x}m`;
+      else if (x < 60 * 24) return `${Math.floor(x / 60)}h`;
+      else return `${Math.floor(x / 60 / 24)}d`;
+    }
+
     $: console.log($people);
   </script>
   
 {#if $people}
   <div class="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3">
-      {#each $people.filter((p: any) => p.name != "updated_at") as person}
+      {#each $people.filter((p) => p.name != "updated_at") as person}
         <div class="flex items-center bg-gray-600 text-zinc-100 p-1 px-3 rounded-md my-1 mx-1">
-          {#if minsAgo(person.time) < 5}
+          {#if minsAgo(person.time) < 8}
             <div class="shrink-0 w-2 h-2 bg-green-600 rounded-lg mr-3"/>
           {:else if minsAgo(person.time) < 15}
             <div class="shrink-0 w-2 h-2 bg-orange-600 rounded-lg mr-3"/>
@@ -25,13 +31,13 @@
           {person.name}
           {#if minsAgo(person.time) >= 5}
             <span class="ml-auto text-zinc-400">
-              {minsAgo(person.time)}m
+              {simpleFormatMins(minsAgo(person.time))}
             </span>
           {/if}
         </div>
       {/each}
   </div>
   <div class="m-1 mt-4 text-zinc-100 text-center">
-    Updated {formatEnd(($people.find((p: any) => p.name == "updated_at")?.time ?? 0) * 1000)} ago.
+    Updated {formatEnd(($people.find((p) => p.name == "updated_at")?.time ?? 0) * 1000)} ago.
   </div>
 {/if}
